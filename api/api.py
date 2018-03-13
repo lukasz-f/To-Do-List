6af4f24ceeca29c138bae2680948c2304ff3587c
lukasz-f <lukaszf6@gmail.com>
2018-03-13 20:13:55 +0100

    add task list operations: return all tasks, create new task

--- api/api.py
+++ api/api.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from flask_restful import Api, Resource, marshal_with, fields
+from flask_restful import Api, Resource, marshal_with, fields, abort, reqparse
+import requests
 from models import TaskModel
-import status
 
 
 class TaskManager:
@@ -10,20 +10,26 @@ class TaskManager:
     def __init__(self):
         self.tasks = {}
 
-    def insert_message(self, task):
+    def get_all_tasks(self):
+        return [v for v in self.tasks.values()]
+
+    def get_task(self, id):
+        return self.tasks[id]
+
+    def insert_task(self, task):
         self.__class__ .last_id += 1
         task.id = self.__class__.last_id
         self.tasks[task.id] = task
 
-    def get_message(self, id):
-        return self.tasks[id]
+    def update_task(self, task):
+        self.tasks[task.id] = task.copy()
 
-    def delete_message(self, id):
+    def delete_task(self, id):
         del self.tasks[id]
 
 
 task_manager = TaskManager()
-task_manager.insert_message(TaskModel('task'))
+task_manager.insert_task(TaskModel('task'))
 
 task_fields = {
     'id': fields.Integer,
@@ -31,21 +37,53 @@ task_fields = {
 }
 
 
+class Index(Resource):
+    def get(self):
+        return 'To-Do List REST app'
+
+
 class TaskList(Resource):
     @marshal_with(task_fields)
     def get(self):
-        return [v for v in task_manager.tasks.values()]
+        return task_manager.get_all_tasks()
+
+    @marshal_with(task_fields)
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', type=int)
+        parser.add_argument('content', type=str)
+        args = parser.parse_args()
+        task = TaskModel(
+            content=args['content']
+        )
+        task_manager.insert_task(task)
+        return task, requests.codes.created
 
 
-class Index(Resource):
-    def get(self):
-        return 'Flask app'
+class Task(Resource):
+    def abort_if_task_doesnt_exist(self, id):
+        if id not in task_manager.tasks:
+            abort(requests.codes.not_found, message="Message {0} doesn't exist".format(id))
+
+    @marshal_with(task_fields)
+    def get(self, id):
+        self.abort_if_task_doesnt_exist(id)
+        return task_manager.get_task(id)
+
+#     @marshal_with(task_fields)
+#     def patch(self, id):
+#         pass
+#
+    def delete(self, id):
+        self.abort_if_task_doesnt_exist(id)
+        return task_manager.delete_task(id), requests.codes.no_content
 
 
 app = Flask(__name__)
 api = Api(app)
 api.add_resource(Index, '/api')
 api.add_resource(TaskList, '/api/tasks')
+# api.add_resource(Task, '/api/task/<int:id>')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
