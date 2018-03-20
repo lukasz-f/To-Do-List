42fcfd6f05ec6a2433a0453f7bec314a029e4aca
lukasz-f <lukaszf6@gmail.com>
2018-03-20 23:32:45 +0100

    add creation date and completed to task model. Model update with patch method

--- api/api.py
+++ api/api.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_restful import Api, Resource, marshal_with, fields, abort, reqparse
 import requests
+from datetime import datetime
+from pytz import timezone
 from models import TaskModel
 
 
@@ -25,15 +27,19 @@ class TaskManager:
         self.tasks[task.id] = task.copy()
 
     def delete_task(self, id):
-        del self.tasks[id]
+        del(self.tasks[id])
 
 
 task_manager = TaskManager()
-task_manager.insert_task(TaskModel('task'))
+task_manager.insert_task(TaskModel('task', datetime.now()))
+task_manager.insert_task(TaskModel('task', datetime.utcnow()))
 
 task_fields = {
     'id': fields.Integer,
-    'content': fields.String
+    'uri': fields.Url('message_endpoint'),
+    'content': fields.String,
+    'creation_date': fields.DateTime(dt_format='iso8601'),
+    'completed': fields.Boolean
 }
 
 
@@ -50,11 +56,11 @@ class TaskList(Resource):
     @marshal_with(task_fields)
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('id', type=int)
-        parser.add_argument('content', type=str)
+        parser.add_argument('content', type=str, required=True, help="Content can't be blank")
         args = parser.parse_args()
         task = TaskModel(
-            content=args['content']
+            content=args['content'],
+            creation_date=datetime.now(tz=timezone('Europe/Warsaw'))
         )
         task_manager.insert_task(task)
         return task, requests.codes.created
@@ -63,17 +69,27 @@ class TaskList(Resource):
 class Task(Resource):
     def abort_if_task_doesnt_exist(self, id):
         if id not in task_manager.tasks:
-            abort(requests.codes.not_found, message="Message {0} doesn't exist".format(id))
+            abort(requests.codes.not_found, content="Task {0} doesn't exist".format(id))
 
     @marshal_with(task_fields)
     def get(self, id):
         self.abort_if_task_doesnt_exist(id)
         return task_manager.get_task(id)
 
-#     @marshal_with(task_fields)
-#     def patch(self, id):
-#         pass
-#
+    @marshal_with(task_fields)
+    def patch(self, id):
+        self.abort_if_task_doesnt_exist(id)
+        task = task_manager.get_task(id)
+        parser = reqparse.RequestParser()
+        parser.add_argument('content', type=str)
+        parser.add_argument('completed', type=bool)
+        args = parser.parse_args()
+        if 'content' in args and args['content'] is not None:
+            task.content = args['content']
+        if 'completed' in args and args['completed'] is not None:
+            task.completed = args['completed']
+        return task
+
     def delete(self, id):
         self.abort_if_task_doesnt_exist(id)
         return task_manager.delete_task(id), requests.codes.no_content
@@ -83,7 +99,7 @@ app = Flask(__name__)
 api = Api(app)
 api.add_resource(Index, '/api')
 api.add_resource(TaskList, '/api/v1/tasks')
-# api.add_resource(Task, '/api/v1/task/<int:id>')
+api.add_resource(Task, '/api/v1/task/<int:id>', endpoint='message_endpoint')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- api/models.py
+++ api/models.py
@@ -1,4 +1,6 @@
 class TaskModel:
-    def __init__(self, content):
-        self.id = 0
+    def __init__(self, content, creation_date):
+        self.id = 0  # We will automatically generate the new id
         self.content = content
+        self.creation_date = creation_date
+        self.completed = False
